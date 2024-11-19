67ba40690ca43c9481b82c5f5dcf17c1e542ae7b
kanishka kalra <kanishkakalra5152@gmail>
2024-11-19 16:14:06 -0300

    created my first  small project using state

--- project1/E-signature-App/src/App.jsx
+++ project1/E-signature-App/src/App.jsx
@@ -5,11 +5,25 @@ import './App.css'
 import { Header } from './components/ui'
 
 function App() {
- 
+  const [val, setVal]= useState("")
+  const [date, setDate]= useState()
+
+  const nameChange=(e)=>{
+    // console.log(e.target.value)
+    setVal(e.target.value)
+  }
+  const dateChange=(e)=>{
+    setDate(e.target.value)
+  }
+
   return (
-   <>
-   <Header/>
-   
+<>
+   <Header name={val}/>
+   <Header name = {!date ? "Dob " : date}/>
+   <p className=" text-center mx-36">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsam, necessitatibus suscipit doloribus quo ipsum illo optio nemo assumenda earum esse temporibus, facere aliquid inventore explicabo? Porro architecto cum hic possimus qui obcaecati? Quis ratione, consequatur ipsam impedit saepe, laborum voluptate atque eius debitis minima quas delectus nemo inventore nostrum  </p>
+    <input type="date" value={date} className='' onChange={dateChange}/>
+    <input type="text" value={val} className='border-2 border-dotted outline-none py-2' onChange={nameChange}/>
+  
    </>
   )
 }
--- project1/E-signature-App/src/components/ui.jsx
+++ project1/E-signature-App/src/components/ui.jsx
@@ -1,9 +1,12 @@
-export function Header() {
+import { useState } from "react"
+
+export function Header( {name} ) {
+  
     return (
-      <>
-      <h1>Name</h1>
-      <h2>Dob</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsam, necessitatibus suscipit doloribus quo ipsum illo optio nemo assumenda earum esse temporibus, facere aliquid inventore explicabo? Porro architecto cum hic possimus qui obcaecati? Quis ratione, consequatur ipsam impedit saepe, laborum voluptate atque eius debitis minima quas delectus nemo inventore nostrum sed </p>
-      </>
+   
+      <div className=" m-20 ">
+      <h1 className=" text-center text-5xl ">{!name ? "Name" : name}</h1>
+      </div>
+     
     )
   }
