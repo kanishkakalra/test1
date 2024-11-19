ef14e5237eabd210d88c4313f1cc7d50c50ce92d
kanishka kalra <kanishkakalra5152@gmail>
2024-11-19 12:52:40 -0300

    created my first component

--- project1/E-signature-App/src/App.jsx
+++ project1/E-signature-App/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Header } from './components/ui'
 
 function App() {
  
   return (
-    <>
-    <><h1>hello vite</h1></>
+   <>
+   <Header/>
    
-    </>
+   </>
   )
 }
 
---
+++ project1/E-signature-App/src/components/ui.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+      <>
+      <h1>Name</h1>
+      <h2>Dob</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsam, necessitatibus suscipit doloribus quo ipsum illo optio nemo assumenda earum esse temporibus, facere aliquid inventore explicabo? Porro architecto cum hic possimus qui obcaecati? Quis ratione, consequatur ipsam impedit saepe, laborum voluptate atque eius debitis minima quas delectus nemo inventore nostrum sed </p>
+      </>
+    )
+  }
